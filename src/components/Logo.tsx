5b603f8db589c76b9f9727765af5ab34cc324eb8
redadesigner10 <reda.designer10@gmail.com>
2025-05-20 01:48:03 +0100

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,7 +6,7 @@ const Logo: React.FC = () => {
       <img
         src="https://cdn.jsdelivr.net/gh/redadesigner10/photo@921da29f9249892634d28ec795fb949ab3674b19/aba9e17c-4ed2-4abd-b288-13732206829d.jpeg"
         alt="Princesses Logo"
-        className="h-10 w-10 mr-2 object-contain"
+        className="h-310 w-310 mr-2 object-contain"
       />
       <div className="flex flex-col">
         <span className="font-bold text-xl text-purple-700">Princesses</span>
