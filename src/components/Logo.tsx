287ba0496441a7abd3973e542db431490a22f56f
redadesigner10 <reda.designer10@gmail.com>
2025-05-20 01:51:07 +0100

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,11 +6,11 @@ const Logo: React.FC = () => {
       <img
         src="https://cdn.jsdelivr.net/gh/redadesigner10/photo@921da29f9249892634d28ec795fb949ab3674b19/aba9e17c-4ed2-4abd-b288-13732206829d.jpeg"
         alt="Princesses Logo"
-        className="h-310 w-310 mr-2 object-contain"
+        className="h-15 w-15 mr-2 object-contain"
       />
       <div className="flex flex-col">
         <span className="font-bold text-xl text-purple-700">Princesses</span>
-        <span className="text-xs text-gray-600">Natural oils to treat hair</span>
+        <span className="text-xs text-gray-600">الزيوت الطبيعية لعلاج الشعر</span>
       </div>
     </div>
   );
